e60791ff474eff38c3a0dedfcdb9155c3dc3dc09
Jesuloba <jesulobajohn@gmail.com>
2020-10-06 14:55:51 +0100

    completed the challenge :)

--- main/script.js
+++ main/script.js
@@ -468,7 +468,7 @@ var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
 
 ///////////////////////////////////////////
 // Maps 
-
+/*
 const question = new Map();
 question.set('question', 'What is the official name of the lastest major Javascript version?');
 question.set(1, 'ES5');
@@ -500,20 +500,122 @@ for (let [key, value] of question.entries()) {
 const ans = parseInt(prompt('Write the correct answer')); 
 
 console.log(question.get(ans === question.get('correct'))); 
+*/
 
 
 
+////////////////////////////////////////////
+// Classes
+
+/*
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('John', 1999, 'teacher');
+
+
+// ES6
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    
+    calculateAge() {
+        const age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+    
+    static greeting() {
+        console.log('Hey there!');
+    }
+}
+
+const john6 = new Person6('John', 1997, 'teacher');
+
+Person6.greeting();
+*/
 
 
 
+/////////////////////////////////////////
+// Classes and Subclasses
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+// Subclass 
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+}
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log('won a new medal: ' + this.medals + ' medals now');
+}
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+ 
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
 
 
 
+// ES6
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    
+    calculateAge() {
+        const age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
 
+// Subclass 
+class Athlete6 extends Person5 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    
+    wonMedal() {
+        this.medals++;
+        console.log('won a new medal: ' + this.medals + ' medals now');
+    }
+}
 
+const johnAthlete6 = new Athlete6('John', 1995, 'sprinter', 3, 9);   
 
-
-
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
 
 
 
